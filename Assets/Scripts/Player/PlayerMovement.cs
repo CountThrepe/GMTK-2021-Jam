7f762e78d3dbfd37c7382cd85f9a56ff28265955
Nate <nathanhaashoward@gmail.com>
2021-06-09 23:34:49 -0600

    Added hopefully useful starting stuff

---
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed = 5;
+
+    private Vector2 move;
+    private Rigidbody2D rb;
+    void Start() {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        rb.velocity = move;
+    }
+
+    public void OnMove(InputValue value)
+    {
+        move = value.Get<Vector2>() * speed;
+    }
+}
---
+++ Assets/Scripts/UI/MainMenuHandler.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuHandler : MonoBehaviour
+{
+    public Animator animator;
+    public GameObject fg;
+    public float transDuration = 1f;
+
+    private float transEnd;
+
+    void Start()
+    {
+        transEnd = -1;
+    }
+
+    void Update()
+    {
+        if(transEnd != -1)
+        {
+            if(Time.time > transEnd) SceneManager.LoadScene(1);
+        }
+    }
+
+    public void StartTransition()
+    {
+        // fg.SetActive(true);
+        // animator.SetTrigger("Hush");
+        transEnd = Time.time + transDuration;
+    }
+}
